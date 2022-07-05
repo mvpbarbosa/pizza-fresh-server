2df7eeb731c9493dca5d312cb05189253a43e83b
Marcos Vinicius <mvpbarbosa16@gmail.com>
2022-07-05 15:19:37 -0300

    feat(table): create dto 'CreateTableDto'

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTableDto } from './dto/create-table.dto';
 
 @Injectable()
 export class TableService {
   findAll() {
     return 'Buscar todas as mesas';
   }
-  create() {
-    return 'Criar uma mesa';
+  create(createTableDto: CreateTableDto) {
+    return 'Criar uma mesa: ' + JSON.stringify(createTableDto);
   }
 }
